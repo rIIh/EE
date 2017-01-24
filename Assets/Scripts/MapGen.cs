2aaa882f6e8c85fb1ebae217000c21217e50eea4
rIIh <ratehavchik@gmail.com>
2017-01-24 16:53:13 +0300

    Mechanics almost done

--- Assets/Scripts/MapGen.cs
+++ Assets/Scripts/MapGen.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class MapGen : MonoBehaviour
 {
-    
+
     public int levelSize;
     public float tileSize;
     public GameObject tile;
@@ -11,9 +11,14 @@ public class MapGen : MonoBehaviour
     Vector3[] orientation = new Vector3[4];
     public List<Vector3> tileCoords = new List<Vector3>();
     public List<Vector3> treeRoots = new List<Vector3>();
+    public List<Vector3> linkCoords = new List<Vector3>();
     Vector3 currentOrient;
     Vector3 wannaBeLoc;
     public GameObject root;
+    public GameObject link;
+    List<GameObject> links = new List<GameObject>();
+    List<GameObject> roots = new List<GameObject>();
+
     void ResetVars()
     {
         tileCoords.Clear();
@@ -23,6 +28,7 @@ public class MapGen : MonoBehaviour
         orientation[3] = new Vector3(0, 0, -tileSize);
         tile.transform.localScale = new Vector3(tileSize / 100, (float)0.1, tileSize / 100);
         root.transform.localScale = new Vector3(tileSize / 100, (float)0.2, tileSize / 100);
+        link.transform.localScale = new Vector3(tileSize / 10, (float)0.1, tileSize / 10);
     }
 
     void PlaceTile(Vector3 curLoc)
@@ -35,28 +41,44 @@ public class MapGen : MonoBehaviour
 
     void PlaceDebugRoot(Vector3 Location)
     {
-        root = Instantiate(root, Location , new Quaternion());
+        root = Instantiate(root, Location, new Quaternion());
+        roots.Add(root);
         root.name = root.name.Replace("(Clone)", "");
         root.transform.parent = gameObject.transform;
+    }
+
+    void PlaceDebugLink(Vector3 Location)
+    {
+        link = Instantiate(link, Location, new Quaternion());
+        links.Add(link);
+        link.name = link.name.Replace("(Clone)", "");
+        link.transform.parent = gameObject.transform;
     }
 
     void Start()
     {
         ResetVars();
         PlaceTile(new Vector3(0, 0, 0));
-        generateFirstBranch();
+        generateFirstBranch(new Vector3(0,0,0), levelSize);
         PlaceRootPoints();
-        GenerateBranches();
+        GenerateBranches(levelSize);
+        PlaceLinks();
+        LinkTiles();
+        linkCoords.Clear();
+        PlaceLinks();
+        GenerateExtraBranches(3);
+        linkCoords.Clear();
+        PlaceLinks();
+        LinkTiles();
     }
 
 
-    void generateFirstBranch()
+    void generateFirstBranch(Vector3 currentPos, int size)
     {
-        currentPos = new Vector3(0, 0, 0);
         currentOrient = new Vector3(0, 0, 0);
-        for (int i = 0; i < levelSize - 1; i++)
-        {
-
+        var safe = 0;
+        for (int i = 0; i < size - 1; i++)
+        { 
             int orInd = Random.Range(0, 99) % 4;
             currentOrient = orientation[orInd];
             wannaBeLoc = currentPos + currentOrient;
@@ -67,12 +89,12 @@ public class MapGen : MonoBehaviour
                 orInd = 0;
             }
 
-
             if (
                 !tileCoords.Contains(wannaBeLoc) &&
                 !tileCoords.Contains(wannaBeLoc + orientation[orInd]) &&
                 !tileCoords.Contains(wannaBeLoc - orientation[orInd]) &&
-                (!tileCoords.Contains(wannaBeLoc + currentOrient - orientation[orInd]) ||
+                (!tileCoords.Contains(wannaBeLoc + currentOrient - orientation[orInd])
+                ||
                 !tileCoords.Contains(wannaBeLoc + currentOrient + orientation[orInd]))
                 )
             {
@@ -82,35 +104,36 @@ public class MapGen : MonoBehaviour
             else
             {
                 i--;
+                safe++;
+                if(safe > 40) { break; }
                 continue;
             }
-
         }
     }
 
     void PlaceRootPoints()
     {
-        foreach(Vector3 curTile in tileCoords)
+        foreach (Vector3 curTile in tileCoords)
         {
             if (
-                curTile != new Vector3(0,0,0) &&
+                curTile != new Vector3(0, 0, 0) &&
 
                 !treeRoots.Contains(curTile + orientation[0]) &&
                 !treeRoots.Contains(curTile + orientation[1]) &&
                 !treeRoots.Contains(curTile + orientation[2]) &&
                 !treeRoots.Contains(curTile + orientation[3]) &&
 
-                !treeRoots.Contains(curTile + orientation[0]+orientation[1]) &&
-                !treeRoots.Contains(curTile + orientation[1]+orientation[2]) &&
-                !treeRoots.Contains(curTile + orientation[2]+orientation[3]) &&
-                !treeRoots.Contains(curTile + orientation[3]+orientation[0]) &&
+                !treeRoots.Contains(curTile + orientation[0] + orientation[1]) &&
+                !treeRoots.Contains(curTile + orientation[1] + orientation[2]) &&
+                !treeRoots.Contains(curTile + orientation[2] + orientation[3]) &&
+                !treeRoots.Contains(curTile + orientation[3] + orientation[0]) &&
 
-                !treeRoots.Contains(curTile + 2*orientation[0]) &&
-                !treeRoots.Contains(curTile + 2*orientation[1]) &&
-                !treeRoots.Contains(curTile + 2*orientation[2]) &&
-                !treeRoots.Contains(curTile + 2*orientation[3]) &&
+                !treeRoots.Contains(curTile + 2 * orientation[0]) &&
+                !treeRoots.Contains(curTile + 2 * orientation[1]) &&
+                !treeRoots.Contains(curTile + 2 * orientation[2]) &&
+                !treeRoots.Contains(curTile + 2 * orientation[3]) &&
 
-                Random.Range(0,100) > 30
+                Random.Range(0, 100) > 30
                )
             {
                 treeRoots.Add(curTile);
@@ -120,15 +143,14 @@ public class MapGen : MonoBehaviour
 
     }
 
-    void GenerateBranches()
+    void GenerateBranches(int branchSize)
     {
-        foreach(Vector3 curRoot in treeRoots)
+        foreach (Vector3 curRoot in treeRoots)
         {
             currentPos = curRoot;
             int safe = 0;
-            for(int i = 0; i < levelSize - 1; i++)
+            for (int i = 0; i < branchSize - 1; i++)
             {
-
                 int orInd = Random.Range(0, 99) % 4;
                 currentOrient = orientation[orInd];
                 wannaBeLoc = currentPos + currentOrient;
@@ -160,13 +182,135 @@ public class MapGen : MonoBehaviour
                     }
                     continue;
                 }
-
-
             }
         }
     }
 
+    void PlaceLinks()
+    {
+        int i;
+        Vector3 placeLoc;
+        foreach (Vector3 loc in tileCoords)
+        {
+            var ind = 0; 
+            foreach (Vector3 orient in orientation)
+            {
+                i = ind + 1;
+                if (i == 4) { i = 0; }
+                if (
+                    tileCoords.Contains(loc + orient) &&
+                    tileCoords.Contains(loc - orient) &&
+                    !tileCoords.Contains(loc + orientation[i]) &&
+                    !tileCoords.Contains(loc - orientation[i])
+                    )
+                {
+                    if (
+                        !tileCoords.Contains(loc + orientation[i] + orient) &&
+                        !tileCoords.Contains(loc + orientation[i] - orient) &&
+                        !tileCoords.Contains(loc + orientation[i]) &&
+
+                        !linkCoords.Contains(loc + orient - orientation[i]) &&
+                        !linkCoords.Contains(loc + orient + orientation[i]) &&
+                        !linkCoords.Contains(loc - orient + orientation[i]) &&
+                        !linkCoords.Contains(loc - orient - orientation[i])
+                        )
+                    {
+                        placeLoc = loc + orientation[i];
+                        if (linkCoords.Contains(placeLoc))
+                        {
+                        }
+                        else
+                        {
+                            PlaceDebugLink(placeLoc);
+                            linkCoords.Add(placeLoc);
+                        }
+                    }
+
+
+                    if (
+                            !tileCoords.Contains(loc - orientation[i] + orient) &&
+                            !tileCoords.Contains(loc - orientation[i] - orient) &&
+                            !tileCoords.Contains(loc - orientation[i]) &&
+
+                            !linkCoords.Contains(loc + orient - orientation[i]) &&
+                            !linkCoords.Contains(loc + orient + orientation[i]) &&
+                            !linkCoords.Contains(loc - orient + orientation[i]) &&
+                            !linkCoords.Contains(loc - orient - orientation[i])
+                        )
+                    {
+                        placeLoc = loc - orientation[i];
+                        if (linkCoords.Contains(placeLoc))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            PlaceDebugLink(placeLoc);
+                            linkCoords.Add(placeLoc);
+                            break;
+                        }
+
+                    }
+                    
+                } else { print(false); }
+                ind++;
+            }
+        }
+    }
+
+    void LinkTiles()
+    { var ind = 0;
+        foreach (Vector3 curPos in linkCoords)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                var orient = orientation[i];
+                var side = i + 1;
+                if (side == 4) { side = 0; }
+                if (
+                        (
+                        tileCoords.Contains(curPos + orient + orientation[side]) &&
+                        tileCoords.Contains(curPos + orient) &&
+                        tileCoords.Contains(curPos + orient - orientation[side]) &&
+                        tileCoords.Contains(curPos - orient + orientation[side]) &&
+                        tileCoords.Contains(curPos - orient) &&
+                        tileCoords.Contains(curPos - orient - orientation[side])
+                        )
+                    ||
+                        (
+                        tileCoords.Contains(curPos + orient + orientation[side]) &&
+                        tileCoords.Contains(curPos + orient - orientation[side]) &&
+                        tileCoords.Contains(curPos + orient)
+                        &&
+                        (tileCoords.Contains(curPos - orient - orientation[side]) 
+                        ||
+                        tileCoords.Contains(curPos - orient + orientation[side]))
+                        &&
+                        tileCoords.Contains(curPos - orient) &&
+                        !tileCoords.Contains(curPos + orientation[side]) &&
+                        !tileCoords.Contains(curPos - orientation[side])
+                        )
+                    )
+                {
+                    print(true);
+                    PlaceTile(curPos);
+                    break;
+                }
+            }
+            ind++;
+        }
+    }
+
+    void GenerateExtraBranches(int size)
+    {
+        foreach(Vector3 curLoc in linkCoords)
+        {
+            PlaceTile(curLoc);
+            generateFirstBranch(curLoc, size);
+        }
+    }
 }
+
 
 
 
